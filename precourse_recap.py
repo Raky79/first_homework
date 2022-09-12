d2f17ca691bd023a01ff1ebd9ff4bbb1cbca16da
Raquel Revelles <raquel@Raquels-MacBook-Pro.local>
2022-09-12 19:35:33 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,11 @@
+concert_tickets=60
+name="Erik"
+
+def greet(name):
+  print("Hello"+" "+name+", "+"How much are the tickets?")
+greet("Erik")
+if concert_tickets < 50: 
+  print("Sure"+" "+name+", "+"you can go")
+else:
+ print("Too Expensive, sorry,maybe next time")
+ print("C'est la Vie!")
